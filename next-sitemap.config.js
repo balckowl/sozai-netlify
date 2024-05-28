2eb76dc5e4ec4122a2d8831ddbbe15b9f37d53e4
balckowl <shiggyjr1492@gmail.com>
2024-05-28 15:53:56 +0900

    最新の記事に修正しました。

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// next-sitemap.config.json
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https: //example.com',
+    generateRobotsTxt: true,
+}
--- sitemap.config.js
+++
@@ -1,9 +0,0 @@
-// sitemap.config.js
-
-
-module.exports = {
-    siteUrl: 'https://soz-ai.com',
-    changefreq: 'weekly',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-};
