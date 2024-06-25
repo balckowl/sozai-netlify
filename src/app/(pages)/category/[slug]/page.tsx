9529430cf44f6f36af369e11b664626a204799a9
balckowl <shiggyjr1492@gmail.com>
2024-06-25 22:58:52 +0900

    feat: static export

--- src/app/(pages)/category/[slug]/page.tsx
+++ src/app/(pages)/category/[slug]/page.tsx
@@ -29,7 +29,7 @@ export const generateMetadata = async ({ params }: { params: { slug: string } })
 }
 
 //ssgの設定
-export const dynamicParams = false
+export const dynamic = 'force-static'
 
 export async function generateStaticParams() {
 
