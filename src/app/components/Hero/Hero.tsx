2b7102bed52c0bb972cbd61ed5b57be9ccb8fdaa
balckowl <shiggyjr1492@gmail.com>
2024-08-12 15:46:06 +0900

    feat: メイン画像をスマホ用にも作成

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -4,7 +4,8 @@ const Hero = async () => {
 
     return (
         <div>
-            <Image src="/images/hero-4.png" width={1200} height={700} alt="hero" className="w-full h-full object-cover" priority/>
+            <Image src="/images/hero-4.png" width={1200} height={700} alt="hero" className="w-full h-full object-cover hidden md:block" priority/>
+            <Image src="/images/hero-5-sm.png" width={1200} height={700} alt="hero" className="w-full h-full object-cover md:hidden" priority/>
         </div>
     )
 }
